7107d76b8c51a1aa49a72d94e1dc143adc1b1a2a
Pablo Sebastian Serfaty <psebaspier@hotmail.com>
2024-05-14 10:26:24 -0300

    Update Caso_2.py

--- Caso_2.py
+++ Caso_2.py
@@ -3,123 +3,122 @@ import numpy as np
 import scipy.stats as stats
 import pandas as pd
 
-def chi_squared_test(numbers: list, num_bins=10, alpha=0.05):
+def prueba_chi_cuadrado(numeros: list, cantidad_bins=10, nivel_significancia=0.05):
     """
     Realiza el test de chi-cuadrado para evaluar si una secuencia de números sigue una distribución uniforme.
-    
+
     Args:
-        numbers (list): Secuencia de números generados.
-        num_bins (int): Número de bins para el histograma.
-        alpha (float): Nivel de significancia para el test de chi-cuadrado.
+        numeros (list): Secuencia de números generados.
+        cantidad_bins (int): Número de bins para el histograma.
+        nivel_significancia (float): Nivel de significancia para el test de chi-cuadrado.
 
     Returns:
         bool: True si pasa el test, False de lo contrario.
         float: Estadístico de chi-cuadrado calculado.
         float: Valor crítico de chi-cuadrado.
     """
-    expected_frequency = len(numbers) / num_bins
-    observed_frequency, _ = np.histogram(numbers, bins=num_bins)
-    chi_squared_statistic = np.sum((observed_frequency - expected_frequency) ** 2 / expected_frequency)
-    critical_value = stats.chi2.ppf(1 - alpha, num_bins - 1)
-    return chi_squared_statistic <= critical_value, chi_squared_statistic, critical_value
+    frecuencia_esperada = len(numeros) / cantidad_bins
+    frecuencia_observada, _ = np.histogram(numeros, bins=cantidad_bins)
+    estadistico_chi_cuadrado = np.sum((frecuencia_observada - frecuencia_esperada) ** 2 / frecuencia_esperada)
+    valor_critico = stats.chi2.ppf(1 - nivel_significancia, cantidad_bins - 1)
+    return estadistico_chi_cuadrado <= valor_critico, estadistico_chi_cuadrado, valor_critico
 
-def congruential_mixed(seed: int, a=16807, c=32, m=2147483647, iterations=100, quant_digits=123):
+def generador_aleatorio_mixto(semilla: int, coeficiente_multiplicador=16807, incremento=32, modulo=2147483647, iteraciones=100, cantidad_digitos=123):
     """
     Genera una secuencia de números pseudoaleatorios utilizando el método congruencial mixto.
 
     Args:
-        seed (int): Semilla inicial para el generador de números aleatorios.
-        a (int): Multiplicador.
-        c (int): Incremento.
-        m (int): Módulo.
-        iterations (int): Número de iteraciones para generar la secuencia.
-        quant_digits (int): Cantidad de dígitos a generar.
+        semilla (int): Semilla inicial para el generador de números aleatorios.
+        coeficiente_multiplicador (int): Multiplicador.
+        incremento (int): Incremento.
+        modulo (int): Módulo.
+        iteraciones (int): Número de iteraciones para generar la secuencia.
+        cantidad_digitos (int): Cantidad de dígitos a generar.
 
     Returns:
         list: Secuencia de números pseudoaleatorios generados.
     """
-    results = []
-    for _ in range(iterations):
-        seed = (a * seed + c) % m
-        seed += datetime.datetime.now().microsecond
-        for digit in str(seed):
-            results.append(int(digit))
-            if len(results) == quant_digits:
-                return results
-    return results
+    resultados = []
+    for _ in range(iteraciones):
+        semilla = (coeficiente_multiplicador * semilla + incremento) % modulo
+        semilla += datetime.datetime.now().microsecond
+        for digit in str(semilla):
+            resultados.append(int(digit))
+        if len(resultados) == cantidad_digitos:
+            return resultados
+    return resultados
 
-def get_numeros_indices(probabilidad):
+def obtener_rangos_probabilidad(probabilidad_defecto):
     """
     Obtiene los rangos de índices para clasificar los números generados como defectuosos o no defectuosos.
 
     Args:
-        probabilidad (float): Probabilidad de defecto en la producción.
+        probabilidad_defecto (float): Probabilidad de defecto en la producción.
 
     Returns:
         dict: Diccionario con los rangos de índices para clasificar los números.
         int: Valor máximo posible de un número generado.
     """
-    numero = str(probabilidad)
-    numeros_despues_del_punto = numero.split(".")[1]
-    maximo = "".join(["9" for _ in range(len(numeros_despues_del_punto))])
-    maximo = int(maximo)
-    rangos = {
+    numero_str = str(probabilidad_defecto)
+    numeros_despues_del_punto = numero_str.split(".")[1]
+    valor_maximo = int("".join(["9" for _ in range(len(numeros_despues_del_punto))]))
+    rangos_clasificacion = {
         "defectuoso": [0, int(numeros_despues_del_punto) - 1],
-        "no_defectuoso": [int(numeros_despues_del_punto), maximo]
+        "no_defectuoso": [int(numeros_despues_del_punto), valor_maximo]
     }
-    return rangos, maximo
+    return rangos_clasificacion, valor_maximo
 
-def simulador_lote(p, n, a, seed=12344):
+def simular_control_lote(probabilidad_lote, tamaño_muestra, limite_aceptacion, semilla=12344):
     """
     Simula el proceso de control de calidad para un lote de placas de video.
 
     Args:
-        p (float): Probabilidad de defecto en la producción.
-        n (int): Tamaño de la muestra de control.
-        a (int): Límite de aceptación para la proporción de placas defectuosas.
-        seed (int): Semilla inicial para el generador de números aleatorios.
+        probabilidad_lote (float): Probabilidad de defecto en la producción para el lote actual.
+        tamaño_muestra (int): Tamaño de la muestra de control.
+        limite_aceptacion (int): Límite de aceptación para la proporción de placas defectuosas.
+        semilla (int): Semilla inicial para el generador de números aleatorios.
 
     Returns:
         bool: True si el lote es aprobado, False si es rechazado.
     """
-    rangos_probabilidad, maximo = get_numeros_indices(p)
-    defectuosas = 0
-    no_defectuosas = 0
-    numeros_generados = congruential_mixed(seed=seed, quant_digits=n * len(str(maximo)) * 2)
-    validacion, _, _ = chi_squared_test(numeros_generados)
-    while not validacion:
-        seed += 1
-        numeros_generados = congruential_mixed(seed=seed, quant_digits=n * len(str(maximo)) * 2)
-        validacion, _, _ = chi_squared_test(numeros_generados)
-    for _ in range(n):
-        numero = int("".join(str(numeros_generados.pop(0)) for _ in range(len(str(maximo)))))
-        if rangos_probabilidad["defectuoso"][0] <= numero <= rangos_probabilidad["defectuoso"][1]:
-            defectuosas += 1
+    rangos_probabilidad, valor_maximo = obtener_rangos_probabilidad(probabilidad_lote)
+    cantidad_defectuosas = 0
+    cantidad_no_defectuosas = 0
+    numeros_aleatorios = generador_aleatorio_mixto(semilla=semilla, cantidad_digitos=tamaño_muestra * len(str(valor_maximo)) * 2)
+    prueba_valida = False
+    while not prueba_valida:
+        semilla += 1
+        numeros_aleatorios = generador_aleatorio_mixto(semilla=semilla, cantidad_digitos=tamaño_muestra * len(str(valor_maximo)) * 2)
+        prueba_valida, _, _ = prueba_chi_cuadrado(numeros_aleatorios)
+    for _ in range(tamaño_muestra):
+        numero_generado = int("".join(str(numeros_aleatorios.pop(0)) for _ in range(len(str(valor_maximo)))))
+        if rangos_probabilidad["defectuoso"][0] <= numero_generado <= rangos_probabilidad["defectuoso"][1]:
+            cantidad_defectuosas += 1
         else:
-            no_defectuosas += 1
-    if defectuosas <= a:
+            cantidad_no_defectuosas += 1
+    if cantidad_defectuosas <= limite_aceptacion:
         return True
     else:
         return False
 
-def simulador_global_electricity(cantidad_lotes):
+def simular_control_fabrica(cantidad_simulaciones):
     """
     Simula el proceso de control de calidad para múltiples lotes de placas de video.
 
     Args:
-        cantidad_lotes (int): Cantidad de lotes a simular.
+        cantidad_simulaciones (int): Cantidad de lotes a simular.
     """
-    lotes = []
+    lista_lotes = []
     print("Ingresar los valores de p, n y a para la simulación de la fábrica de placas de video")
-    p = float(input("Probabilidad de placa grafica defectuosa (En decimales): "))
-    n = int(input("Tamaño de la muestra de control: "))
-    a = int(input("Límite de aceptación (Del tamaño de la muestra actual): "))
-    for _ in range(cantidad_lotes):
-        lotes.append(simulador_lote(p=0.23, n=60, a=15, seed=333))
-    lotes = pd.DataFrame(lotes, columns=["Aprobado"])
-    print(lotes)
-    print(lotes["Aprobado"].value_counts())
-    print(lotes["Aprobado"].value_counts(normalize=True))
-    print("Proporción de placas defectuosas promedio:", lotes["Aprobado"].mean())
+    probabilidad_lote = float(input("Probabilidad de placa grafica defectuosa (En decimales): "))
+    tamaño_muestra = int(input("Tamaño de la muestra de control: "))
+    limite_aceptacion = int(input("Límite de aceptación (Del tamaño de la muestra actual): "))
+    for _ in range(cantidad_simulaciones):
+        lista_lotes.append(simular_control_lote(probabilidad_lote, tamaño_muestra, limite_aceptacion, semilla=333))
+    datos_lotes = pd.DataFrame(lista_lotes, columns=["Aprobado"])
+    print(datos_lotes)
+    print(datos_lotes["Aprobado"].value_counts())
+    print(datos_lotes["Aprobado"].value_counts(normalize=True))
+    print("Proporción de placas defectuosas promedio:", datos_lotes["Aprobado"].mean())
 
-simulador_global_electricity(10)
+simular_control_fabrica(6)
